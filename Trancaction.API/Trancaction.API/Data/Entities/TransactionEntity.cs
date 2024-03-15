0451c5ff223cb9b48e358ae9bb2bad8913c81dbd
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-15 12:07:34 +0200

    add Entity and configuration

--- Trancaction.API/Trancaction.API/Data/Entities/TransactionEntity.cs
+++ Trancaction.API/Trancaction.API/Data/Entities/TransactionEntity.cs
@@ -2,5 +2,11 @@
 {
     public class TransactionEntity
     {
+        public string Transaction_id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public DateTime Transaction_date { get; set; }
+        public string Client_location { get; set; } = null!;
     }
 }
---
+++ Trancaction.API/Trancaction.API/Data/EntityConfiguration/TransactionEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Trancaction.API.Data.EntityConfiguration
+{
+    public class TransactionEntityConfiguration : IEntityTypeConfiguration<TransactionEntity>
+    {
+        public void Configure(EntityTypeBuilder<TransactionEntity> builder)
+        {
+            builder.ToTable("Transaction");
+
+            builder.HasKey(c => c.Transaction_id);
+
+            builder.Property(e => e.Transaction_id)
+                .IsRequired();
+
+            builder.Property(e => e.Name)
+                 .IsRequired();
+
+            builder.Property(e => e.Email)
+                 .IsRequired();
+
+            builder.Property(e => e.Amount)
+                .IsRequired()
+                .HasColumnType("decimal(18, 2)");    
+
+            builder.Property(e => e.Transaction_date)
+                .IsRequired();
+
+            builder.Property(e => e.Client_location)
+                .IsRequired();
+        }
+    }
+}
