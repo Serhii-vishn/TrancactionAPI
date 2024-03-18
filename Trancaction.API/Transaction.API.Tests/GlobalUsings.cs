599ad57e011224c0fcc71beafac09ae7b38cc1b3
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-18 11:03:22 +0200

    Delete Trancaction.API/Transaction.API.Tests directory

--- Trancaction.API/Transaction.API.Tests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Transaction.API.Services.Interfaces;
-global using Moq;
-global using FluentAssertions;
-global using System.Data.Common;
-global using Transaction.API.Data;
-global using Transaction.API.Services;
